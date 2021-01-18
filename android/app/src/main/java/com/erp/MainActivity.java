c06104361c4841f0cfcbecb0f30495a50e4a5058
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2021-01-18 19:03:45 +0530

    Splash Screen

--- android/app/src/main/java/com/erp/MainActivity.java
+++ android/app/src/main/java/com/erp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.erp;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
--- android/app/src/main/java/com/erp/MainApplication.java
+++ android/app/src/main/java/com/erp/MainApplication.java
@@ -7,8 +7,10 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 public class MainApplication extends Application implements ReactApplication {
