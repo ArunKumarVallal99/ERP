035f90843f67ac7cd40cd9d1b532a6534bc18d26
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2021-01-18 19:20:57 +0530

    Splash Screen

--- android/app/src/main/java/com/erp/MainApplication.java
+++ android/app/src/main/java/com/erp/MainApplication.java
@@ -24,15 +24,15 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(),
-            new SplashScreenReactPackage()  //here 
-            );
-          // @SuppressWarnings("UnnecessaryLocalVariable")
-          // List<ReactPackage> packages = new PackageList(this).getPackages();
-          // // Packages that cannot be autolinked yet can be added manually here, for example:
-          // // packages.add(new MyReactNativePackage());
-          // return packages;
+          // return Arrays.<ReactPackage>asList(
+          //           new MainReactPackage(),
+          //   new SplashScreenReactPackage()  //here 
+          //   );
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
         }
 
         @Override
