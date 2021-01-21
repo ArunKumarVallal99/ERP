fdf6222b75dedb134df2bd19e78baee42d1f45f4
Shyam-25 <shyam.s@optisolbusiness.com>
2021-01-21 18:16:14 +0530

    sprint plan 1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,31 +3,27 @@ import { Text, View, StyleSheet, TextInput, TouchableOpacity, Alert} from 'react
 import styles from './Style';
 
 export default class Login extends Component{
-    state={
-        email:''
+    state = {
+        email:'',
+        passwordValue:''
     };
 
     validation=()=>{
         const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        // if(!this.state.email.trim()){
-        //     console.log('missing email')
-        //     Alert.alert('missing email')
-        // }
-        // else if(reg.test(this.state.email)=== false){
-        //     Alert.alert("Invalid Email ID")
-        //   } 
-    //     if (reg.test(this.state.email) === true){
-    //         alert('valid');
-    //     }
-    //     else{
-    //         alert();
-    //     }
-    // }
-    // onLogin() {
-    //     const { username} = this.state;
+        if(!this.state.email.trim()){
+            console.log('Missing email')
+            Alert.alert('Missing email')
+        }
+        else if(reg.test(this.state.email)){
+            Alert.alert("Invalid Email ID")
+          } 
+          else if(!this.state.passwordValue.trim()){
+            console.log("Missing Password")
+            Alert.alert("Missing Password")
+          }
+       
+    }
     
-    //     Alert.alert('Credentials', `${username} `);
-    //   }
     render(){
         return(
             <View style={styles.container}>
@@ -40,7 +36,7 @@ export default class Login extends Component{
                         placeholder='optisolbusiness@gmail.com'
                         textContentType='password'
                         secureTextEntry={true}
-                        onChangeText={text=>this.setState({emailValue:text})} />
+                        onChangeText={text=>this.setState({email:text})} />
                         {/* <Text>Fiii</Text> */}
                         {/* // underlineColorAndroid='grey' /> */}
                     </View>
@@ -51,12 +47,17 @@ export default class Login extends Component{
                         keyboardType='visible-password'
                         placeholder='optisolbusiness'
                         textContentType='password'
-                        secureTextEntry={true} />
+                        secureTextEntry={true} 
+                        onChangeText={text=>this.setState({passwordValue: text})}
+                        />
+                        <TouchableOpacity>
                         <Text style={styles.forg}>Forgot Password.?</Text>
+                        </TouchableOpacity>
                         {/* // underlineColorAndroid='grey' /> */}
                      </View>
                     <View style={styles.buttonView}>
-                    <TouchableOpacity style={styles.button}>
+                    <TouchableOpacity style={styles.button}
+                            onPress={this.validation} >
                     <Text style={styles.buttonText}>Sign In</Text>
                     </TouchableOpacity>
                     </View> 
