328da8db505722dd8bae550caaf6d52eba5fff22
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2021-02-01 18:53:49 +0530

    Sprint plan 1

--- App.js
+++ App.js
@@ -2,11 +2,8 @@
 import React, { Component } from 'react'
 import {View,Text} from 'react-native';
 import Login from './src/views/Login/Login';
-import PermissionApply from './src/views/PermissionApply/PermissionApply';
 import SplashScreen from 'react-native-splash-screen'
 import Bottom from './src/navigation/Bottom';
-import ChangePassword from './src/views/Change password/ChangePassword';
-
 class App extends Component{
   
   componentDidMount() {
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< Updated upstream
 import { Text, View, TextInput, TouchableOpacity, Alert, Modal, Image} from 'react-native';
 import styles from './styles';
-
-=======
-import { Text, View, TextInput, TouchableOpacity, Alert, Modal, } from 'react-native';
-import styles from './styles';
->>>>>>> Stashed changes
-
 export default class Login extends Component{
     state = {
         email:'',
